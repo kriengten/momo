b463c4ab3ba79dcdee17dc9490ac13164c4d18cf
kriengten <kriengten@gmail.com>
2022-02-02 17:55:05 +0700

    Update krieng.py

--- krieng.py
+++ krieng.py
@@ -13,7 +13,7 @@ def get_platform():
 
     return platforms[sys.platform]
 
-def read_db_config(filename='../Dropbox/config.ini', section='mysql'):
+def read_db_config(filename='../../Dropbox/config.ini', section='mysql'):
     """ Read database configuration file and return a dictionary object
     :param filename: name of the configuration file
     :param section: section of database configuration
